d42ab23b86acde3e89f92974bca0c8d1f263605d
Shannon Houser <shannon.houser@summitllc.us>
2023-04-25 09:45:20 -0400

    Formatted code

--- BlackLung_Dashboard/server/home.R
+++ BlackLung_Dashboard/server/home.R
@@ -1,68 +1,68 @@
 output$home1 <- renderValueBox({
   valueBox(
-    formatC('10+', format="d", big.mark=',')
-    ,paste(("Map Visualizations"))
-    ,icon = icon("map-marked-alt")
-    ,color = "black"
-    ,width = 8)
+    formatC('30+', format="d", big.mark=','),
+    paste(("Possible Map Visualizations")),
+    icon = icon("map-marked-alt"),
+    color = "black",
+    width = 8)
 })
 
 output$home2 <- renderValueBox({
   valueBox(
-    formatC(nrow(data), format="d", big.mark=',')
-    ,paste(("Counties Represented"))
-    ,icon = icon('map-pin')
-    ,color = "navy"
-    ,width = 4)
+    formatC(nrow(data), format="d", big.mark=','),
+    paste(("Counties Represented")),
+    icon = icon('map-pin'),
+    color = "navy",
+    width = 4)
 })
 
 output$home3 <- renderValueBox({
   valueBox(
-    formatC(sum(data$total_black_lung_deaths), format="d", big.mark=',')
-    ,paste(('Total Black Lung Deaths'))
-    ,icon = icon('lungs')
-    ,color = "yellow"
-    ,width = 12)
+    formatC(sum(data$total_black_lung_deaths), format="d", big.mark=','),
+    paste(('Total Black Lung Deaths')),
+    icon = icon('lungs'),
+    color = "yellow",
+    width = 12)
 })
 
 output$home4 <- renderValueBox({
   valueBox(
-    formatC(sum(data$any_cwp), format="d", big.mark=',')
-    ,paste(("Total Black Lung Cases"))
-    ,icon = icon("lungs-virus")
-    ,color = "black")
+    formatC(sum(data$any_cwp), format="d", big.mark=','),
+    paste(("Total Black Lung Cases")),
+    icon = icon("lungs-virus"),
+    color = "black")
 })
 
 output$home5 <- renderValueBox({
   valueBox(
-    formatC(data %>% count(region_id) %>% count() %>% pull() -1, format="d", big.mark=',')
-    ,paste(("Regions Represented"))
-    ,icon = icon('mountain-city')
-    ,color = "navy")
+    formatC(data %>% count(region_id) %>% count() %>% pull() -1, format="d", big.mark=','),
+    paste(("Regions Represented")),
+    icon = icon('mountain-city'),
+    color = "navy")
 })
 
 output$home6 <- renderValueBox({
   valueBox(
-    formatC(data %>% count(msha_district_county) %>% count() %>% pull() - 1, format="d", big.mark=',')
-    ,paste(('MSHA Districts Represented'))
-    ,icon = icon('hard-hat')
-    ,color = "yellow")
+    formatC(data %>% count(msha_district_county) %>% count() %>% pull() - 1, format="d", big.mark=','),
+    paste(('MSHA Districts Represented')),
+    icon = icon('hard-hat'),
+    color = "yellow")
 })
 
 output$home7 <- renderValueBox({
   valueBox(
-    formatC(data %>% filter(any_cwp != 0) %>% group_by(state) %>% count() %>% ungroup() %>% count() %>% ungroup(), format="d", big.mark=',')
-    ,paste(("States with Black Lung Cases"))
-    ,icon = icon("flag-usa")
-    ,color = "black")
+    formatC(data %>% filter(any_cwp != 0) %>% group_by(state) %>% count() %>% ungroup() %>% count() %>% ungroup(), format="d", big.mark=','),
+    paste(("States with Black Lung Cases")),
+    icon = icon("flag-usa"),
+    color = "black")
 })
 
 output$home8 <- renderValueBox({
   valueBox(
-    formatC(sum(data$totalmines21), format="d", big.mark=',')
-    ,paste(("Total Mines in 2021"))
-    ,icon = icon('hard-hat')
-    ,color = "navy")
+    formatC(sum(data$totalmines21), format="d", big.mark=','),
+    paste(("Total Mines in 2021")),
+    icon = icon('hard-hat'),
+    color = "navy")
 })
 
 
