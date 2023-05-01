e1954cf1fcdb1f357cced6bf79af392229f1a837
Shannon Houser <shannon.houser@summitllc.us>
2023-05-01 10:51:00 -0400

    aesthetics

--- BlackLung_Dashboard/global.R
+++ BlackLung_Dashboard/global.R
@@ -18,6 +18,27 @@ map_data <- data %>%
   st_as_sf(crs = 4326)
 # geo join
 
+
+factors <- c('countyfips', 
+             'countyname', 
+             'state', 
+             'msha_district_county', 
+             'region_id', 
+             'surfacemines01', 
+             'undergrndmines01', 
+             'totalmines01', 
+             'surfacemines21', 
+             'undergrndmines21', 
+             'totalmines21', 
+             'pct_total_mining_employees_1986', 
+             'pct_total_mining_employees_2020', 
+             'total_weekly_exposure_hours', 
+             'totalprodx1000st01', 
+             'totalprodx1000st21', 
+             'scalar_coal_county', 
+             'scalar_current_coal_county', 
+             'scalar_former_coal_county')
+
 variables <- colnames(map_data)[map_data %>% colnames() %>% str_detect("per1000|pct|percent|tot")]
 variables <- setNames(variables, 
                       colnames(map_data)[map_data %>% colnames() %>%
