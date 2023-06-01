e101d62ccd11a377726a81867b368befd5d1866e
Shannon Houser <shannon.houser@summitllc.us>
2023-06-01 17:00:02 -0400

    Adding missing data

--- preprocessing_map_data.R
+++ preprocessing_map_data.R
@@ -13,7 +13,27 @@ counties <- counties %>%
   rename(countyfips = GEOID) %>% 
   mutate(countyfips = as.double(countyfips))
 
+# Add missing counties
+missing = data.frame(
+  countyfips = c(20005, 10005, 10001, 10003, 20003, 20001, 20007, 20009, 08014),
+  countyname = c("Atchison", "Sussex", "Kent", "New Castle", "Anderson", "Allen", "Barber", "Barton", "Broomfield"),
+  state = c("Kansas", "Delaware", "Delaware", "Delaware", "Kansas", "Kansas", "Kansas", "Kansas", "Colorado"),
+  region_id = c("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+  msha_district_county = c(NA, NA, NA, NA, NA, NA, NA, NA, NA)
+)
+
+# Replace NA to match rest of data
+data <- data %>% 
+  bind_rows(missing) %>% 
+  mutate(across(is.numeric, ~replace_na(., 0))) %>% 
+  mutate(across(is.character, ~replace_na(., "N/A")))
+
 map_data <- data %>% 
+  mutate(countyfips = case_when(
+    countyfips == 12025 ~ 12086,
+    countyfips == 46113 ~ 46102,
+    TRUE ~ countyfips
+  )) %>% 
   left_join(counties) %>% 
   st_as_sf(crs = 4326)
 # geo join
@@ -25,3 +45,8 @@ map_data <- map_data %>%
 # Would need to presave this and then read it in for it to be worth it
 map_data2 <- ms_simplify(map_data, keep = 0.01, keep_shapes = TRUE)
 write_rds(map_data2, "BlackLung_Dashboard/counties_lite.rds")
+
+# Navajo 
+navajo <- sf::read_sf('BlackLung_Dashboard/cb_2018_us_aiannh_500k/cb_2018_us_aiannh_500k.shp') %>% filter(NAME == "Navajo Nation")
+navajo2 <- ms_simplify(navajo, keep = 0.01, keep_shapes = TRUE)
+write_rds(navajo2, "BlackLung_Dashboard/navajo.rds")
