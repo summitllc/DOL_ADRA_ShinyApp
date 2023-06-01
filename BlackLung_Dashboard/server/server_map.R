06972a960ddaa74c7ccd0923463f8cb2778ba66f
Shannon Houser <shannon.houser@summitllc.us>
2023-06-01 17:01:27 -0400

    Map tab updates (aesthetic)

--- BlackLung_Dashboard/server/server_map.R
+++ BlackLung_Dashboard/server/server_map.R
@@ -7,22 +7,26 @@ library(rmapshaper)
 
 val <- eventReactive(input$search_map, {
   val <- input$factor
-}) 
+})
+
+# met <- eventReactive(input$search_map, {
+#   met <- input$metric
+# })
 
 map_data2 <- eventReactive(input$search_map, {
-  # map_data %>% 
-  #   filter(get(val()) > 0)
+  # map_data %>%
+  #   filter(get(val()) > 0 & get(met()) > 0)
   map_data
-}) 
+})
 
 output$map <- renderLeaflet({
   # Creating a color palette based on the number range in the column of interest
   pal <- colorNumeric("Blues", domain = map_data2()[[val()]], na.color = "#696969")
-  
+
   if (val() %in% percent_cols) {
-    popup_sb <- paste0("<B><u>", map_data2()$countyname, " County, ", map_data2()$state, "</B></u>", 
+    popup_sb <- paste0("<B><u>", map_data2()$countyname, " County, ", map_data2()$state, "</B></u>",
                        "<br>",
-                       "Region: ", map_data2()$region_id, 
+                       "Region: ", map_data2()$region_id,
                        "<br>",
                        "MSHA District: ", map_data2()$msha_district_county,
                        "<br>",
@@ -35,7 +39,7 @@ output$map <- renderLeaflet({
   } else {
     popup_sb <- paste0("<B><u>", map_data2()$countyname, " County, ", map_data2()$state, "</B></u>",
                        "<br>",
-                       "Region: ", map_data2()$region_id, 
+                       "Region: ", map_data2()$region_id,
                        "<br>",
                        "MSHA District: ", map_data2()$msha_district_county,
                        "<br>",
@@ -46,10 +50,11 @@ output$map <- renderLeaflet({
                        "Total Black Lung Deaths: ", map_data2()$total_black_lung_deaths
     )
   }
-  
-  map <- leaflet() %>% 
-    addProviderTiles("CartoDB.Positron") %>%
-    setView(-98.35, 39.5, zoom = 5) %>% 
+
+  map <- leaflet() %>%
+    # always groups
+    addProviderTiles("CartoDB.Positron", group = "Tiles") %>%
+    setView(-98.35, 39.5, zoom = 5) %>%
     addPolygons(data = map_data2(),
                 fillColor = ~pal(map_data2()[[val()]]),
                 stroke = TRUE,
@@ -58,99 +63,113 @@ output$map <- renderLeaflet({
                 fillOpacity = .9,
                 weight = 0.2,
                 smoothFactor = 0.2,
-                popup = ~popup_sb) %>%
+                popup = ~popup_sb, 
+                group = "County-level Data") %>%
+    # overlay groups
     addPolygons(data = navajo, color = "#FF0000", weight = 1, smoothFactor = 0.5,
-                opacity = 1.0, fill = FALSE) %>% 
+                opacity = 1.0, fill = FALSE, group = "Navajo Nation") %>%
     addPolygons(data = appalachia, color = "#FF0000", weight = 1, smoothFactor = 0.5,
-                opacity = 1.0, fill = FALSE) %>% 
+                opacity = 1.0, fill = FALSE, group = "Appalachia") %>%
+    # Overlay control
+    addLayersControl(
+      # baseGroups = c("Tiles"),
+      overlayGroups = c("Navajo Nation", "Appalachia"),
+      options = layersControlOptions(collapsed = FALSE)
+    ) %>% 
+    # Legend for map
     addLegend(pal = pal,
               values = map_data2()[[val()]],
               position = "bottomleft",
-              title = names(variables)[variables==val()])
-  
-  map
+              title = names(variables)[variables==val()]) #%>%
+    # addCircles(lng = ~long, lat = ~lat, weight = 1,
+    #            radius = ~map_data2()[[met()]],
+    #            data = map_data2)
+
+  map %>% 
+    hideGroup("Navajo Nation") %>% 
+    hideGroup("Appalachia")
 })
 
-output$incid_plot <- renderPlot({
-  map_data2() %>% 
-    ggplot(aes_string(x = val(), y = 'any_cwp')) +
-    geom_point() +
-    geom_smooth(method = "lm",
-                color = "#fa234b") +
-    theme_bw() +
-    labs(title = paste(names(variables)[variables==val()], 
-                       " vs. \n",
-                       'Total Black Lung Incidences'),
-         x = names(variables)[variables==val()],
-         y = 'Total Black Lung Incidences')
-})
-
-output$death_plot <- renderPlot({
-  map_data2() %>% 
-    ggplot(aes_string(x = val(), y = 'total_black_lung_deaths')) +
-    geom_point() +
-    geom_smooth(method = "lm",
-                color = "#fa234b") +
-    theme_bw() +
-    labs(title = paste(names(variables)[variables==val()],
-                       " vs. \n",
-                       names(variables)[variables=='total_black_lung_deaths']),
-         x = names(variables)[variables==val()],
-         y = names(variables)[variables=='total_black_lung_deaths'])
-})
+# output$incid_plot <- renderPlot({
+#   map_data2() %>%
+#     ggplot(aes_string(x = val(), y = 'any_cwp')) +
+#     geom_point() +
+#     geom_smooth(method = "lm",
+#                 color = "#fa234b") +
+#     theme_bw() +
+#     labs(title = paste(names(variables)[variables==val()],
+#                        " vs. \n",
+#                        'Total Black Lung Incidences'),
+#          x = names(variables)[variables==val()],
+#          y = 'Total Black Lung Incidences')
+# })
+#
+# output$death_plot <- renderPlot({
+#   map_data2() %>%
+#     ggplot(aes_string(x = val(), y = 'total_black_lung_deaths')) +
+#     geom_point() +
+#     geom_smooth(method = "lm",
+#                 color = "#fa234b") +
+#     theme_bw() +
+#     labs(title = paste(names(variables)[variables==val()],
+#                        " vs. \n",
+#                        names(variables)[variables=='total_black_lung_deaths']),
+#          x = names(variables)[variables==val()],
+#          y = names(variables)[variables=='total_black_lung_deaths'])
+# })
 
 observeEvent(input$search_map, {
   output$region_data <- renderDataTable({
-    df <- map_data %>% 
-      st_drop_geometry() %>% 
-      mutate(intermed = round(get(val()), 2)) %>% 
+    df <- map_data %>%
+      st_drop_geometry() %>%
+      mutate(intermed = round(get(val()), 2)) %>%
       select(countyname, state, region_id, msha_district_county, intermed, any_cwp, total_black_lung_deaths) %>%
-      arrange(desc(intermed)) %>% 
+      arrange(desc(intermed)) %>%
       rename(County = countyname,
              State = state,
              Region = region_id,
              `MSHA District` = msha_district_county,
              `Number of Black Lung Cases` = any_cwp,
              `Number of Black Lung Deaths` = total_black_lung_deaths
-      ) 
-    
+      )
+
     colnames(df)[5] <- names(variables)[variables==val()]
     df
-    
+
   }, escape = F, options = list(lengthMenu = c(10, 25, 50), pageLength = 10))
-  
+
 })
 
 # output$state_data <- renderDataTable({
-#   map_data %>% 
-#     st_drop_geometry() %>% 
-#     group_by(state) %>% 
+#   map_data %>%
+#     st_drop_geometry() %>%
+#     group_by(state) %>%
 #     summarise(`Number of Mines (2001)` = sum(totalmines01),
 #               `Number of Mines (2021)` = sum(totalmines21),
 #               `Number of Black Lung Cases` = sum(any_cwp),
-#               `Number of Black Lung Deaths` = sum(total_black_lung_deaths)) %>% 
+#               `Number of Black Lung Deaths` = sum(total_black_lung_deaths)) %>%
 #     rename(State = state)
 # }, escape = F, options = list(lengthMenu = c(5, 15, 25), pageLength = 5))
 
 # output$region_data <- renderDataTable({
-#   map_data %>% 
-#     st_drop_geometry() %>% 
-#     group_by(region_id) %>% 
+#   map_data %>%
+#     st_drop_geometry() %>%
+#     group_by(region_id) %>%
 #     summarise(`Number of Mines (2001)` = sum(totalmines01),
 #               `Number of Mines (2021)` = sum(totalmines21),
 #               `Number of Black Lung Cases` = sum(any_cwp),
 #               `Number of Black Lung Deaths` = sum(total_black_lung_deaths)
-#     ) %>% 
+#     ) %>%
 #     rename(Region = region_id)
 # }, escape = F, options = list(lengthMenu = c(5, 15, 25), pageLength = 5))
-# 
+#
 # output$state_data <- renderDataTable({
-#   map_data %>% 
-#     st_drop_geometry() %>% 
-#     group_by(state) %>% 
+#   map_data %>%
+#     st_drop_geometry() %>%
+#     group_by(state) %>%
 #     summarise(`Number of Mines (2001)` = sum(totalmines01),
 #               `Number of Mines (2021)` = sum(totalmines21),
 #               `Number of Black Lung Cases` = sum(any_cwp),
-#               `Number of Black Lung Deaths` = sum(total_black_lung_deaths)) %>% 
+#               `Number of Black Lung Deaths` = sum(total_black_lung_deaths)) %>%
 #     rename(State = state)
 # }, escape = F, options = list(lengthMenu = c(5, 15, 25), pageLength = 5))
--- BlackLung_Dashboard/ui/ui_map.R
+++ BlackLung_Dashboard/ui/ui_map.R
@@ -1,57 +1,66 @@
 tab_map <- tabItem(tabName = "map",
-                   
-                   titlePanel(p(style="text-align: center;", ("Explore the data with our Interactive Map!"))),
-                   
+
+                   titlePanel(p(style="text-align: center;", ("Explore the data with our interactive map!"))),
+
                    hr(),
-                   
-                   h4(p(style="text-align: center;", ("This tab helps you visualize the Black Lung Dataset to explore patterns and geospatial patterns."))),
-                   
+
+                   h4(p(style="text-align: center;", ("This tab helps you visualize the geographic aspects of the black lung dataset."))),
+
                    br(),
-                   
+
                    box(title = strong(("Search Factors related to Black Lung")), status = "success", width = 12,
-                       
-                       
+
+
                        fluidRow(
-                         
+
                          column(4,
                                 selectInput(
                                   inputId = "factor",
-                                  label = ("Choose a Factor"),
+                                  label = strong(("Choose a Factor and click Explore!")),
                                   choices = variables
                                   # choices = colnames(map_data)[map_data %>% colnames() %>% str_detect("per1000|pct|percent|tot")]
                                 )),
-                         
+
+                         # column(4,
+                         #        selectInput(
+                         #          inputId = "metric",
+                         #          label = ("Choose a Metric"),
+                         #          choices = metrics
+                         #          # choices = colnames(map_data)[map_data %>% colnames() %>% str_detect("per1000|pct|percent|tot")]
+                         #        )),
+
                          column(2,
-                                br(),
+                                p(" ", style = "margin-bottom: 25px;"),
                                 actionButton(inputId = "search_map",
                                              label = strong(("Explore!")))
                          )
                        )
-                       
+
                    ),
-                   
+
                    fluidRow(
                      column(12,
-                            leafletOutput(outputId = "map", height = 800)
+                            withSpinner(leafletOutput(outputId = "map", height = 800), type = 4, color = "black")
                             )),
-                   
+
                    br(),
-                   
+
                    fluidRow(
                      column(12,
                             dataTableOutput(outputId = "region_data"))
                      # column(6,
                      #        dataTableOutput(outputId = "state_data"))
-                   ),
-                   
-                   br(),
-                   
-                   fluidRow(
-                     column(6,
-                            plotOutput(outputId = "incid_plot")),
-                     column(6,
-                            plotOutput(outputId = "death_plot")))
-                   
-                  
-        
+                   )
+
+                   # br(),
+                   #
+                   # fluidRow(
+                   #   column(6,
+                   #          plotOutput(outputId = "incid_plot")),
+                   #   column(6,
+                   #          plotOutput(outputId = "death_plot")))
+
+
+
 )
+
