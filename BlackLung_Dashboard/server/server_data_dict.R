fd3556a1a2cd889a6aa1dadb59fa3545417ad85b
Shannon Houser <shannon.houser@summitllc.us>
2023-07-25 15:09:41 -0400

    Update data dictionary to new version

--- BlackLung_Dashboard/server/server_data_dict.R
+++ BlackLung_Dashboard/server/server_data_dict.R
@@ -1,19 +1,19 @@
 output$data_dict_table <- renderDataTable(
   data_dict %>% 
-    slice(match(variables, Name)) %>% 
+    slice(match(variables, Variable)) %>% 
     bind_rows(data_dict %>% 
-                filter(!(Name %in% variables))) %>% 
-    mutate(Name = str_replace_all(Name, "_", " "),
-           Name = str_replace(Name, "pct", "Percent"),
-           Name = str_replace(Name, "acshouseheatingfueltotalocc", "acs house heating fuel total occ"),
-           Name = str_replace(Name, "acshouseheatingfulepercentu", "acs house heating fuel percent"),
-           Name = str_replace(Name, "totalmines", "total mines 20"),
-           Name = str_replace(Name, "totalprodx1000st", "total production (x1000) 20"),
-           Name = str_replace(Name, "per1000", "(per 1000)"),
-           Name = str_replace(Name, "undrgrnd", "Underground"),
-           Name = str_to_title(Name),
-           Name = str_replace(Name, "Acs", "ACS"),
-           Name = str_replace(Name, "Cwppmf", "CWP PMF"),
-           Name = str_replace(Name, "Cwp", "CWP"),
-           Name = str_replace(Name, "Occ", "OCC"))
+                filter(!(Variable %in% variables))) 
+    # mutate(Name = str_replace_all(Name, "_", " "),
+    #        Name = str_replace(Name, "pct", "Percent"),
+    #        Name = str_replace(Name, "acshouseheatingfueltotalocc", "acs house heating fuel total occ"),
+    #        Name = str_replace(Name, "acshouseheatingfulepercentu", "acs house heating fuel percent"),
+    #        Name = str_replace(Name, "totalmines", "total mines 20"),
+    #        Name = str_replace(Name, "totalprodx1000st", "total production (x1000) 20"),
+    #        Name = str_replace(Name, "per1000", "(per 1000)"),
+    #        Name = str_replace(Name, "undrgrnd", "Underground"),
+    #        Name = str_to_title(Name),
+    #        Name = str_replace(Name, "Acs", "ACS"),
+    #        Name = str_replace(Name, "Cwppmf", "CWP PMF"),
+    #        Name = str_replace(Name, "Cwp", "CWP"),
+    #        Name = str_replace(Name, "Occ", "OCC"))
 )
