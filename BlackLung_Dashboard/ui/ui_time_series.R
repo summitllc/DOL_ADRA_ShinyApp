bcdc4cd86c67dfa40fb22e106eee572c1f180ed9
Shannon Houser <shannon.houser@summitllc.us>
2023-08-07 14:15:10 -0400

    completely re-do additional graphs

--- BlackLung_Dashboard/ui/ui_time_series.R
+++ BlackLung_Dashboard/ui/ui_time_series.R
@@ -2,45 +2,51 @@ library(billboarder)
 
 tab_time_series <- tabItem(tabName = "time_series",
                            
-                           titlePanel(p(style="text-align: center;", ("Explore how mining factors have changed over time!"))),
+                           titlePanel(p(style="text-align: center;", ("Explore the MSHA mining Districts"))),
                            
                            hr(),
                            
-                           h4(p(style="text-align: center;", ("This tab helps you visualize how the number of mines, the percentage of miners, and coal production has changed across time."))),
-                           
+                           # h4(p(style="text-align: center;", ("This tab helps you visualize how mining districts differ."))),
+                           # 
                            br(),
                            
                            fluidPage(
                            
-                           box(title = strong(("How has the mining environment changed over the years?")), status = "success", width = 12,
+                           box(title = strong(("Below we compare black lung prevalence and mining efficiency in MSHA districts:")), status = "success", width = 12,
                                
                              fluidRow(
                                
-                               column(6,
-                                      billboarderOutput(outputId = "surface_time")
-                                      #plotOutput(outputId = "surface_time")#, hover = hoverOpts("surface_time_hover")),
+                               column(12,
+                                      # billboarderOutput(outputId = "surface_time")
+                                      plotOutput(outputId = "surface_time")#, hover = hoverOpts("surface_time_hover")),
                                       #uiOutput("hover_surface_time")
-                               ),
-                               
-                               column(6,
-                                      billboarderOutput(outputId = "under_time")#, hover = hoverOpts("under_time_hover")),
-                                      #uiOutput("hover_under_time")
                                )
+                               
+                               # column(6,
+                               #        billboarderOutput(outputId = "under_time")#, hover = hoverOpts("under_time_hover")),
+                               #        #uiOutput("hover_under_time")
+                               # )
                                
                              ),
                              
                              br(),
                              
                              fluidRow(
-                               column(6,
-                                      billboarderOutput(outputId = "total_mine_time")#, hover = hoverOpts("total_mine_time_hover")),
+                               column(12,
+                                      # billboarderOutput(outputId = "total_mine_time")
+                                      plotOutput(outputId = "total_mine_time")#, hover = hoverOpts("surface_time_hover")),
                                       #uiOutput("hover_total_mine_time")
-                               ),
-                               
-                               column(6,
-                                      billboarderOutput(outputId = "total_prod_time")#, hover = hoverOpts("total_prod_time_hover")),
-                                      #uiOutput("hover_total_prod_time")
                                )
+                               
+                               # column(6,
+                               #        billboarderOutput(outputId = "total_prod_time")#, hover = hoverOpts("total_prod_time_hover")),
+                               #        #uiOutput("hover_total_prod_time")
+                               # )
+                             ),
+                             
+                             fluidRow(
+                               column(12, 
+                                      plotOutput(outputId = "total_employ_time"))
                              )
                            )
                            
