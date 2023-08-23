6ab42acf3fc3d7cf2de73066b26d4fb122376a54
shannon-houser <shannon.houser@summitllc.us>
2023-08-23 14:37:40 -0400

    Added download button for data

--- BlackLung_Dashboard/server/server_data_disclosures.R
+++ BlackLung_Dashboard/server/server_data_disclosures.R
@@ -1,3 +1,15 @@
+output$data_download <- downloadHandler(
+  
+  filename = function() {
+    paste("blacklung_dataset", "xlsx", sep=".")
+  },
+  content = function(file) {
+    
+    file.copy('black lung analytic dataset FOR MAP.xlsx', file,overwrite = TRUE)
+  },
+  contentType = "text/xlsx"
+)
+
 output$text <- renderText({
   "Insert Paragraphs? from Kyle"
 })
--- BlackLung_Dashboard/ui/ui_data_disclosures.R
+++ BlackLung_Dashboard/ui/ui_data_disclosures.R
@@ -2,11 +2,11 @@ tab_data_disclosures <- tabItem(tabName = "data_disclosures",
                          
                          titlePanel(p(style="text-align: center;", ("Data Disclosures"))),
                          
-                         hr(),
-                         
                          h4(p(style="text-align: center;", ("This tab helps you understand the data sources, calculations, and modeling used to create this dashboard."))),
                          
                          br(),
+                         
+                         downloadButton(outputId = "data_download", label = "Data Download"),
                          
                          textOutput(outputId = "text")
                          
